b5129bc38a8d6b50d2a87fd495370ed30575216d
Matdos99 <98588593+Matdos99@users.noreply.github.com>
2022-02-15 09:48:06 +0100

    modifica .gitignore

---
+++ s4/g1/assets/js/myscript.js
@@ -0,0 +1,9 @@
+document.write("la mia voglia");
+document.getElementById("huno").innerHTML = 3 + 5;
+window.alert("mi chiamo Mateos");
+console.log(20 + 15);
+
+function changeColor() {
+    document.getElementById("huno").style.color = "green";
+
+}
