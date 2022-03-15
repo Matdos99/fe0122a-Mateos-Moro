3ca14d9deb99d2924501d6e55416b3ff7b85285a
Matdos99 <98588593+Matdos99@users.noreply.github.com>
2022-03-15 18:32:54 +0100

    m

--- s7/g2/script.js
+++
@@ -1,7 +0,0 @@
-fetch('https://sofin.wp-admin.it/public/api/v1/user'
-)
-    .then(response => response.json())  //trasmorma la risposta in json
-    .then(data => {                 //quando hai finito di trasformare il json
-
-    })
-
